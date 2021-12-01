2a6263f730d628355e538ce1e823d9b2bcc5e1bc
protarie125 <proarie@gmail.com>
2021-12-02 07:55:56 +0900

    초기 커밋.

---
+++ BOJ_11382/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    long long A, B, C;
+    cin >> A >> B >> C;
+    cout << A + B + C;
+
+    return 0;
+}
